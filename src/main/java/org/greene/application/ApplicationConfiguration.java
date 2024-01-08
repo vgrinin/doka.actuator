878961c91543976e65cd859c4f23ecf1cf4eda30
Vyacheslav Grinin <vyacheslav.grinin@ptnl.moscow>
2024-01-08 19:48:45 +0300

    Init spring boot with actuator

---
+++ src/main/java/org/greene/application/ApplicationConfiguration.java
@@ -0,0 +1,12 @@
+package org.greene.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class ApplicationConfiguration extends SpringBootServletInitializer {
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationConfiguration.class, args);
+    }
+}
