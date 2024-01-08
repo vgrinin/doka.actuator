fc347f493e85ffa7d4e6c0f32d27686297e57956
Vyacheslav Grinin <vyacheslav.grinin@ptnl.moscow>
2024-01-08 20:58:49 +0300

    Add build- and git- info

--- src/main/java/org/greene/application/Application.java
+++ src/main/java/org/greene/application/Application.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ApplicationConfiguration extends SpringBootServletInitializer {
+public class Application extends SpringBootServletInitializer {
     public static void main(String[] args) {
-        SpringApplication.run(ApplicationConfiguration.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
